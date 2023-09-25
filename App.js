a6b614fb59c18458f82d133d483ad5de88f9bd95
alswn <minju139@naver.com>
2023-09-25 22:45:32 +0900

    gps 코드 추가

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import LocationContext from './LocationContext';
 import LoginScreen from './Login';
 import Home from './HomeBottom';
 import JoinScreen from './Join';
@@ -8,14 +9,18 @@ import JoinScreen from './Join';
 const Stack = createStackNavigator();
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Join" component={JoinScreen} />
-        {/* 추가 화면을 여기에 추가할 수 있습니다 */}
-      </Stack.Navigator>
-    </NavigationContainer>
+    <LocationContext.Provider value={{ location, setLocation }}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Join" component={JoinScreen} />
+          {/* 추가 화면을 여기에 추가할 수 있습니다 */}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </LocationContext.Provider>
   );
 }
