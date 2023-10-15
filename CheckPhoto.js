9b8ad8d11d3e72a119c76ed8b64dfb2352e40963
seoyun <parkysys17@gmail.com>
2023-10-15 21:59:09 +0900

    사진 촬영 결과 수정

--- CheckPhoto.js
+++ CheckPhoto.js
@@ -1,40 +1,141 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Image, Text, StyleSheet } from 'react-native';
-import { Button } from 'react-native-paper'; // react-native-paper의 Button을 불러옵니다.
+import { Button } from 'react-native-paper';
+import { ScrollView } from 'react-native-gesture-handler';
 
 function CheckPhoto({ route, navigation }) {
   const { photo } = route.params;
+  const [currentPage, setCurrentPage] = useState(1);
+  const [trashTypeConfirmed, setTrashTypeConfirmed] = useState(false);
+  const [selectedTrashType, setSelectedTrashType] = useState('종이'); // 초기값을 종이로 설정합니다.
 
-  const description = "이 사진에 대한 설명을 여기에 추가하세요.";
+  const trashTypes = [
+    '비닐',
+    '스티로폼',
+    '금속캔 (알루미늄)',
+    '금속캔 (철)',
+    '종이',
+    '페트병 (무색 단일)',
+    '페트병 (유색 단일)',
+    '플라스틱 (PE)',
+    '플라스틱 (PP)',
+    '플라스틱 (PS)',
+    '유리병 (갈색)',
+    '유리병 (녹색)',
+    '유리병 (투명)',
+    '건전지',
+    '형광등'
+  ];
 
-  // 확인 버튼을 눌렀을 때 Home으로 이동합니다.
+  // 분리수거 방법 정보를 저장하는 객체
+  const disposalMethods = {
+    '비닐': '비닐을 재활용하려면 깨끗하게 씻은 후 재활용 컨테이너에 버려주세요.',
+    '스티로폼': '스티로폼은 재활용하기 어렵습니다. 일반 쓰레기로 버려야 합니다.',
+    '금속캔 (알루미늄)': '알루미늄 캔은 재활용 컨테이너에 버려주세요.',
+    '금속캔 (철)': '철 캔은 재활용 컨테이너에 버려주세요.',
+    '종이': '종이는 깨끗한 상태로 재활용 컨테이너에 버려주세요.',
+    '페트병 (무색 단일)': '무색 페트병은 재활용 컨테이너에 버려주세요.',
+    '페트병 (유색 단일)': '유색 페트병은 재활용 컨테이너에 버려주세요.',
+    '플라스틱 (PE)': 'PE 플라스틱은 재활용 컨테이너에 버려주세요.',
+    '플라스틱 (PP)': 'PP 플라스틱은 재활용 컨테이너에 버려주세요.',
+    '플라스틱 (PS)': 'PS 플라스틱은 재활용하기 어렵습니다. 일반 쓰레기로 버려야 합니다.',
+    '유리병 (갈색)': '갈색 유리병은 재활용 컨테이너에 버려주세요.',
+    '유리병 (녹색)': '녹색 유리병은 재활용 컨테이너에 버려주세요.',
+    '유리병 (투명)': '투명 유리병은 재활용 컨테이너에 버려주세요.',
+    '건전지': '건전지는 특수 폐기물 수거소에 버려야 합니다.',
+    '형광등': '형광등은 특수 폐기물 수거소에 버려야 합니다.'
+  };
+
+  // 사용자가 선택한 쓰레기 종류
+  const [userSelectedTrashType, setUserSelectedTrashType] = useState('');
+
+  const handleNextPage = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
+  const handleRetakePhoto = () => {
+    setTrashTypeConfirmed(false);
+    setSelectedTrashType(''); // 쓰레기 종류 초기화
+    setCurrentPage(2);
+  };
+
   const handleConfirm = () => {
-    navigation.navigate('Home');
+    if (currentPage === 3 || trashTypeConfirmed) {
+      navigation.navigate('Home');
+    }
   };
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }}>
       <Image
         source={{ uri: photo.uri }}
         style={{ width: 300, height: 400 }}
       />
-      <Text style={styles.description}>{description}</Text>
-      {/* children 속성을 사용하여 버튼 내용을 설정 */}
-      <Button mode="contained" onPress={handleConfirm} style={styles.button}>
-        확인
-      </Button>
+      {currentPage === 1 && !trashTypeConfirmed && (
+        <View style={styles.buttonGroup}>
+          <Text style={styles.question}>
+            {selectedTrashType ? `이 쓰레기가 ${selectedTrashType} 이/가 맞나요?` : '이 쓰레기가 어떤 종류인가요?'}
+          </Text>
+          <Button mode="contained" onPress={() => setTrashTypeConfirmed(true)} style={styles.button}>
+            네
+          </Button>
+          <Button mode="contained" onPress={handleRetakePhoto} style={styles.button}>
+            아니요
+          </Button>
+        </View>
+      )}
+      {currentPage === 2 && !trashTypeConfirmed && (
+        <ScrollView style={{ padding: 20, marginBottom: 30 }}>
+          <Text style={styles.question}>
+            이 쓰레기는 어떤 종류인가요?
+          </Text>
+          {trashTypes.map((trashType, index) => (
+            <Button
+              key={index}
+              mode="contained"
+              onPress={() => {
+                setTrashTypeConfirmed(true);
+                setUserSelectedTrashType(trashType);
+                setSelectedTrashType(trashType); // 선택한 쓰레기 종류 설정
+              }}
+              style={[
+                styles.button,
+                index === trashTypes.length - 1 && { marginBottom: 30 } // 마지막 버튼에만 marginBottom 적용
+              ]}
+            >
+              {trashType}
+            </Button>
+          ))}
+        </ScrollView>
+      )}
+      {currentPage === 3 || trashTypeConfirmed && (
+        <View style={styles.buttonGroup}>
+          <Text style={styles.description}>
+            {disposalMethods[selectedTrashType || userSelectedTrashType] || '분리수거 정보가 없습니다.'}
+          </Text>
+          <Button mode="contained" onPress={handleConfirm} style={styles.button}>
+            홈
+          </Button>
+        </View>
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  description: {
+  question: {
     marginTop: 20,
     fontSize: 16,
+    fontWeight: 'bold',
+  },
+  buttonGroup: {
+    flexDirection: 'column',
+    alignItems: 'center',
   },
   button: {
-    marginTop: 20,
-    backgroundColor: '#628F5D'
+    marginTop: 10,
+    backgroundColor: '#74A16F',
+    color: 'black', // 버튼의 글자 색상을 검정색으로 설정
   },
 });
 
