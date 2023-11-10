1f700789f2fac48e7132477eb788d3e99088d4d7
seoyun <parkysys17@gmail.com>
2023-11-10 16:54:03 +0900

    검색 결과 페이지 디자인 완료

--- SearchResultScreen.js
+++ SearchResultScreen.js
@@ -1,124 +1,83 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Modal } from 'react-native';
 import { useRoute } from '@react-navigation/native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import Icon2 from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export default function SearchResultScreen() {
     const route = useRoute();
     const { searchKeyword } = route.params;
     const navigation = useNavigation();
+    const [result, setResult] = useState(null);
+    const [modalVisible, setModalVisible] = useState(false);
 
-    const goToMap = () => {
-        navigation.navigate('지도'); // 'Map' 스크린으로 이동
+    const handleKeyword = () => {
+        setResult("바나나 (음식물)");
+        setModalVisible(true);
     };
 
-    const goToDetail = () => {
-        navigation.navigate('세부사항'); // 'Map' 스크린으로 이동
+    const closeModal = () => {
+        setModalVisible(false);
     };
 
     return (
         <ScrollView
             style={styles.container}
-            contentContainerStyle={{ padding: 0 }} // props 설정해줘야 함, 0 이어도 지우지 않기 , , 
+            contentContainerStyle={{ padding: 0 }}
         >
             <View style={styles.container}>
                 <View style={{ padding: 30 }}>
-                    <Text style={[styles.text, { fontSize: 15 , marginBottom: 30 }]}>검색하신 {searchKeyword} 결과</Text>
+                    <Text style={[styles.text, { fontSize: 15 }]}>
+                        검색하신 '{searchKeyword}' 결과
+                    </Text>
+                </View>
+                <View style={styles.container}>
+                    <Text style={[styles.title, { padding: 10, marginLeft: 20 }]}>
+                        분리수거
+                    </Text>
+                    <View style={[styles.line, { marginTop: 10 }]}></View>
 
-                    {/* 검색 결과 데이터 디자인이에용 ~~ */}
-                    {/* 카테고리 분리수거에 포함되는 검색 결과 */}
-                    <View style={[styles.resultContainer, { padding: 30 }]}>
-                        <Text style={[styles.text, { marginBottom: 20, fontFamily: 'Pretendard-Bold', fontSize: 18 }]}>분리수거</Text>
-                        <View style={[styles.subContainer, { marginBottom: 15 }]}>
-                            <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 10 }}>
-                                <Text style={[styles.text, { fontSize: 15 }]}>제목 1</Text>
-                                <View style={{ flex: 1, alignItems: 'flex-end' }}>
-                                    <Text style={styles.text}>지역</Text>
-                                </View>
-                            </View>
-                            <Text style={[styles.text, { marginStart: 15, marginBottom: 30 }]}>해당 분리수거 사업에 대한 설명</Text>
-                            {/* 버튼 2개 */}
-                            <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 10 }}>
-                                <TouchableOpacity style={styles.btn} onPress={goToDetail} >
-                                    <Text style={[styles.text]}>자세히 보기</Text>
-                                </TouchableOpacity>
-                                <TouchableOpacity onPress={goToMap} style={[styles.btn,{backgroundColor:'#FAEDFF', marginStart:10}]}>
-                                    <Text style={[styles.text]}>정확한 위치</Text>
-                                </TouchableOpacity>
-                            </View>
-                        </View>
-                        {/* 여기까지가 서브컨테이너 1개 */}
-
-                        <View style={[styles.subContainer, { marginBottom: 15 }]}>
-                            <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 10 }}>
-                                <Text style={[styles.text, { fontSize: 15 }]}>제목 2 </Text>
-                                <View style={{ flex: 1, alignItems: 'flex-end' }}>
-                                    <Text style={styles.text}>지역</Text>
-                                </View>
-                            </View>
-                            <Text style={[styles.text, { marginStart: 15, marginBottom: 30 }]}>해당 분리수거 사업에 대한 설명</Text>
-                            {/* 버튼 2개 */}
-                            <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 10 }}>
-                            <TouchableOpacity style={styles.btn} onPress={goToDetail} >
-                                    <Text style={[styles.text]}>자세히 보기</Text>
-                                </TouchableOpacity>
-                                <TouchableOpacity style={[styles.btn,{backgroundColor:'#FAEDFF', marginStart:10}]}>
-                                    <Text style={[styles.text]}>정확한 위치</Text>
-                                </TouchableOpacity>
-                            </View>
-                        </View>
-                    </View>
-
-                    {/* 카테고리 복지에 포함되는 검색 결과 */}
-                    <View style={[styles.resultContainer, { padding: 30, marginTop: 30 }]}>
-                        <Text style={[styles.text, { marginBottom: 20, fontFamily: 'Pretendard-Bold', fontSize: 18 }]}>복지</Text>
-                        <View style={[styles.subContainer, { marginBottom: 15 }]}>
-                            <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 10 }}>
-                                <Text style={[styles.text, { fontSize: 15 }]}>제목 1</Text>
-                                <View style={{ flex: 1, alignItems: 'flex-end' }}>
-                                    <Text style={styles.text}>지역</Text>
-                                </View>
-                            </View>
-                            <Text style={[styles.text, { marginStart: 15, marginBottom: 30 }]}>해당 복지에 대한 설명</Text>
-                            {/* 버튼 2개 */}
-                            <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 10 }}>
-                            <TouchableOpacity style={styles.btn} onPress={goToDetail} >
-                                    <Text style={[styles.text]}>자세히 보기</Text>
-                                </TouchableOpacity>
-                                <TouchableOpacity style={[styles.btn,{backgroundColor:'#FAEDFF', marginStart:10}]}>
-                                    <Text style={[styles.text]}>정확한 위치</Text>
-                                </TouchableOpacity>
-                            </View>
-                        </View>
-                        {/* 여기까지가 서브컨테이너 1개 */}
-
-                        <View style={[styles.subContainer, { marginBottom: 15 }]}>
-                            <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 10 }}>
-                                <Text style={[styles.text, { fontSize: 15 }]}>제목 2</Text>
-                                <View style={{ flex: 1, alignItems: 'flex-end' }}>
-                                    <Text style={styles.text}>지역</Text>
-                                </View>
-                            </View>
-                            <Text style={[styles.text, { marginStart: 15, marginBottom: 30 }]}>해당 복지에 대한 설명</Text>
-                            {/* 버튼 2개 */}
-                            <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 10 }}>
-                            <TouchableOpacity style={styles.btn} onPress={goToDetail} >
-                                    <Text style={[styles.text]}>자세히 보기</Text>
-                                </TouchableOpacity>
-                                <TouchableOpacity style={[styles.btn,{backgroundColor:'#FAEDFF', marginStart:10}]}>
-                                    <Text style={[styles.text]}>정확한 위치</Text>
-                                </TouchableOpacity>
-                            </View>
-                        </View>
-                    </View>
-
-
+                    <TouchableOpacity
+                        style={styles.subContainer}
+                        onPress={handleKeyword}
+                    >
+                        <Text style={styles.text2}>빙그레 바나나 우유 (플라스틱)</Text>
+                        <Icon2
+                            name="arrow-right-bold-circle"
+                            style={styles.icon}
+                        />
+                    </TouchableOpacity>
                 </View>
             </View>
-        </ScrollView >
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={closeModal}
+            >
+                <View style={styles.modalBackground}>
+                    <View style={styles.modalContainer}>
+                        <View style={styles.modalContent}>
+                            <Text style={[styles.title,{marginBottom: 5}]}>빙그레 바나나 우유 (플라스틱)</Text>
+                            <Text style={styles.title}>배출 안내</Text>
+                        </View>
+                        <Text style={styles.modalText}>뚜껑 껍질만 일반 쓰레기로 분리</Text>
+                        <Text style={styles.modalText}>용기는 플라스틱으로 배출</Text>
+                        <TouchableOpacity
+                            onPress={closeModal}
+                            style={[styles.closeButton, { marginRight: 5 }]}
+                        >
+                            <Text style={styles.text}>확인</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </Modal>
+        </ScrollView>
     );
 }
+
 
 const styles = StyleSheet.create({
     container: {
@@ -141,21 +100,12 @@ const styles = StyleSheet.create({
         shadowRadius: 2.0,  // 그림자의 반경
         elevation: 3,        // Android에서 그림자 효과를 제공합니다
     },
-    resultContainer: {
-        backgroundColor: '#F2FFED',
-        padding: 8,
-        borderRadius: 10,
-        width: '100%',
-        alignSelf: 'center',
-    },
     subContainer: {
         backgroundColor: 'white',
-        padding: 15,
-        borderRadius: 10,
-        borderWidth: 1,
         width: '100%',
-        height: 130,
-        alignSelf: 'center',
+        height: 100,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
     },
     btn: {
         backgroundColor: '#EDF3FF',
@@ -166,7 +116,72 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center', // 이게 가운데 정렬임 기억해 ㅡㅡ
     },
+    title: {
+        fontSize: 18,
+        fontFamily: 'Pretendard-Bold',
+    },
     text: {
         fontFamily: 'Pretendard-Regular',
     },
+    text2: {
+        fontFamily: 'Pretendard-Regular',
+        justifyContent: 'center',
+        margin: 40,
+    },
+    line: {
+        borderBottomWidth: 1,  // 선의 두께
+        borderBottomColor: '#B5B5B5',  // 선의 색상
+        width: '100%',
+    },
+    icon: {
+        fontSize: 35,
+        color: '#628F5D',
+        margin: 30,
+    },  
+    modalContainer: {
+        width: '80%',
+        backgroundColor: 'white',
+        borderRadius: 10,
+        alignItems: 'center',
+        paddingBottom: 20,
+    },
+    modalText: {
+        fontSize: 15,
+        fontFamily: 'Pretendard-Regular',
+    },
+    modalContent: {
+        width: '100%',
+        backgroundColor: '#F2FFED',
+        padding: 16,
+        borderRadius: 10,
+        alignItems: 'center',
+        marginTop: 40,
+        marginBottom: 20,
+    },
+    modalBackground: {
+        flex: 1,
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    closeButton: {
+        width: '60%',
+        height: 30,
+        marginTop: 20,
+        backgroundColor: '#F2FFED',
+        borderWidth: 1,
+        alignSelf: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.2,
+        shadowRadius: 2.0,
+        elevation: 3,
+    },
+
 });
